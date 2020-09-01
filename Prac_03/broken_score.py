7181ee011c5a85f4877de3ef8b0deb49e80be47f
RobinBabu <robin.babu@my.jcu.edu.au>
2020-09-01 18:41:01 +1000

    Fixed determine score status with function

---
+++ Prac_03/broken_score.py
@@ -0,0 +1,27 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+# TODO: Fix this!
+
+
+def main():
+
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
+
