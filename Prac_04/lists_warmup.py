84ecbd167ba62d924b56143a4745f8b24cd7f8d8
RobinBabu <robin.babu@my.jcu.edu.au>
2020-09-01 19:52:32 +1000

    warmed with lists

---
+++ Prac_04/lists_warmup.py
@@ -0,0 +1,14 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+
+numbers[0] = "ten"
+print(numbers)
+
+# change last element of numbers to 1
+numbers[-1] = 1
+
+# Sliced all numbers exceot for first two
+numbers[2:]
+
+#Check if 9 is an element
+9 in numbers
